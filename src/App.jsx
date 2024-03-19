7185c627057fbadf17d04d32251de71e729b27e4
Max de Vocht <maxdevocht@hotmail.com>
2024-03-19 12:40:49 +0100

    Added Card
    Added Card jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Card from './components/Card'
 import './App.css'
 
 export default function App() {
   return (
-    <Navbar />
+    <div className="container">
+      <Navbar />
+      <Card />
+    </div>
   )
 }
 
