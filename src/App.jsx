092117bdf33d7f9a41ba23e1eb228b53b086c850
Max de Vocht <maxdevocht@hotmail.com>
2024-03-19 12:29:26 +0100

    Nav styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 import './App.css'
 
+export default function App() {
+  return (
+    <Navbar />
+  )
+}
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <i class="ri-earth-line"></i>
+            <p className="title">my travel journal.</p>
+        </nav>
+    )
+}
